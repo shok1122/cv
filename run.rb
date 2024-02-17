2a1d4d39f10b15f862082ace992029159413c64f
shok1122 <shok1122@zohomail.com>
2024-02-17 12:18:20 +0900

    Added Poster

--- run.rb
+++ run.rb
@@ -23,6 +23,8 @@ class Biblio
     case type
     when 'journal', 'proceedings', 'proceedings_jp' then
       return BiblioPaper::factory(_data)
+    when 'poster' then
+      return BiblioPresentation::factory(_data)
     end
   end
 
@@ -52,6 +54,75 @@ class Biblio
 
   def tba()
     return (@tba == true) ? "(To be appeared)" : ""
+  end
+
+end
+
+class BiblioPresentation < Biblio
+
+  def initialize(_data, _name)
+    @conference = _data['conference']
+    @number = _data['number']
+    @city = _data['city']
+    @country = _data['country']
+    super(_data, _name)
+  end
+
+  def self.factory(_data)
+    case _data['lang']
+    when 'en' then
+      return BiblioPresentationEn.new(_data)
+    when 'jp' then
+      return BiblioPresentationJp.new(_data)
+    end
+  end
+
+  def biblio()
+    return "#{author}#{title}#{conference}#{number}#{city}#{country}#{year}. #{tba}".strip
+  end
+
+  def conference()
+    return @conference.nil? ? "" : ", #{@conference}"
+  end
+
+  def number()
+    return @number.nil? ? "" : ", #{@number}"
+  end
+
+  def city()
+    return @city.nil? ? "" : ", #{@city}"
+  end
+
+  def country()
+    return @country.nil? ? "" : ", #{@country}"
+  end
+
+end
+
+class BiblioPresentationEn < BiblioPresentation
+
+  def initialize(_data)
+    super(_data, @@NAME_EN)
+  end
+
+  def author()
+    return author_en()
+  end
+
+  def conference()
+    return @conference.nil? ? "" : ", <i>#{@conference}</i>"
+  end
+
+end
+
+class BiblioPresentationJp < BiblioPresentation
+
+  def initialize(_data)
+    super(_data, @@NAME_JP)
+  end
+
+  def author()
+    return author_jp()
   end
 
 end
@@ -133,11 +204,13 @@ class XXX
   end
 
   def main()
-    publication = YAML.load_file('./publication.yaml')
+    paper = YAML.load_file('./paper.yaml')
+    journal = paper.find_all { |v| v['type'] == "journal" }.sort{|x| x['year']}.reverse
+    proceedings = paper.find_all { |v| v['type'] == "proceedings" }.sort{|x| x['year']}.reverse
+    proceedings_jp = paper.find_all { |v| v['type'] == "proceedings_jp" }.sort{|x| x['year']}.reverse
 
-    journal = publication.find_all { |v| v['type'] == "journal" }.sort{|x| x['year']}.reverse
-    proceedings = publication.find_all { |v| v['type'] == "proceedings" }.sort{|x| x['year']}.reverse
-    proceedings_jp = publication.find_all { |v| v['type'] == "proceedings_jp" }.sort{|x| x['year']}.reverse
+    presentation = YAML.load_file('./presentation.yaml')
+    poster = presentation.find_all { |v| v['type'] == 'poster' }.sort{ |x| x['year']}.reverse
 
     File.open('./template.erb') do |f|
       text = ERB.new(f.read, trim_mode: '%-').result(binding)
