1cf993b804bec4cc6930c940312fbce9fb4d6d28
shok1122 <shok1122@zohomail.com>
2024-02-17 11:17:20 +0900

    Added BiblioPaper

--- run.rb
+++ run.rb
@@ -6,31 +6,78 @@ class Biblio
   @@RQUOTE = '&rdquo;'
   @@ENDASH = '&ndash;'
 
+  @@NAME_EN = 'S. Kakei'
+  @@NAME_JP = '掛井将平'
+
   def initialize(_data, _name)
     @data = _data
     @title = _data['title']
     @author = _data['author']
-    @journal = _data['journal']
     @year = _data['year']
-    @volume = _data['volume']
-    @number = _data['number']
-    @page = (_data['page']['start'].nil? || _data['page']['start'].nil?) ? nil : _data['page']
     @tba = (_data['tba'].nil? || _data['tba'] == false) ? false : true
-
     @name = _name
   end
 
-  def biblio()
-    return "#{author}#{title}#{journal}#{volume}#{number}#{page}#{year}. #{tba}".strip
+  def self.factory(_data)
+    type = _data['type']
+    case type
+    when 'journal', 'proceedings', 'proceedings_jp' then
+      return BiblioPaper::factory(_data)
+    end
   end
 
   def title()
     return @title.nil? ? "" : ", #{@@LQUOTE}#{@title}#{@@RQUOTE}"
   end
 
-  def author()
+  def author_jp()
     @author.map!{ |x| (x == @name) ? "<u>#{x}</u>" : x }
     return @author.join(", ")
+  end
+
+  def author_en()
+    @author.map!{ |x| (x == @name) ? "<u>#{x}</u>" : x }
+    if @author.length > 2
+      return @author[0..-2].join(', ') + ", and #{@author[-1]}"
+    elsif @author.length == 2
+      return "#{@author[0]} and #{@author[1]}"
+    elsif @author.length == 1
+      return "#{@author[0]}"
+    end
+  end
+
+  def year()
+    return @year.nil? ? "" : ", #{@year}"
+  end
+
+  def tba()
+    return (@tba == true) ? "(To be appeared)" : ""
+  end
+
+end
+
+class BiblioPaper < Biblio
+
+  def initialize(_data, _name)
+    @journal = _data['journal']
+    @volume = _data['volume']
+    @number = _data['number']
+    @page = (_data['page']['start'].nil? || _data['page']['start'].nil?) ? nil : _data['page']
+
+    super(_data, _name)
+  end
+
+  def self.factory(_data)
+    case _data['lang']
+    when 'en' then
+      return BiblioPaperEn.new(_data)
+    when 'jp' then
+      return BiblioPaperJp.new(_data)
+    end
+  end
+
+  def biblio()
+    return "#{author}#{title}#{journal}#{volume}#{number}#{page}#{year}. #{tba}".strip
   end
 
   def journal()
@@ -49,32 +96,15 @@ class Biblio
     return @page.nil? ? "" : ", pp. #{@page["start"]}#{@@ENDASH}#{@page["end"]}"
   end
 
-  def year()
-    return @year.nil? ? "" : ", #{@year}"
-  end
-
-  def tba()
-    return (@tba == true) ? "(To be appeared)" : ""
-  end
-
 end
 
-class BiblioEn < Biblio
+class BiblioPaperEn < BiblioPaper
   def initialize(_data)
-    super(_data, 'S. Kakei')
+    super(_data, @@NAME_EN)
   end
 
   def author()
-    authors = ""
-    @author.map!{ |x| (x == @name) ? "<u>#{x}</u>" : x }
-    if @author.length > 2
-      authors = @author[0..-2].join(', ') + ", and #{@author[-1]}"
-    elsif @author.length == 2
-      authors = "#{@author[0]} and #{@author[1]}"
-    elsif @author.length == 1
-      authors = "#{@author[0]}"
-    end
-    return authors
+    return author_en()
   end
 
   def journal()
@@ -82,22 +112,24 @@ class BiblioEn < Biblio
   end
 end
 
-class BiblioJp < Biblio
+class BiblioPaperJp < BiblioPaper
   def initialize(_data)
-    super(_data, '掛井将平')
+    super(_data, @@NAME_JP)
+  end
+
+  def author()
+    return author_jp()
   end
 end
 
 class XXX
+  def factory(_data)
+    return Biblio::factory(_data)
+  end
+
   def make_biblio(_x)
-    case _x['lang']
-    when 'en' then
-      this = BiblioEn.new(_x)
-      return this.biblio()
-    when 'jp' then
-      this = BiblioJp.new(_x)
-      return this.biblio()
-    end
+    this = factory(_x)
+    return this.biblio()
   end
 
   def main()
