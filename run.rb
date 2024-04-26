743e371acd508bec12f4d826e4de72ed6130bbf8
shok1122 <shok1122@zohomail.com>
2024-04-27 07:25:24 +0900

    Changed how index.html is saved

--- run.rb
+++ run.rb
@@ -333,7 +333,9 @@ class XXX
 
     File.open('./template.erb') do |f|
       text = ERB.new(f.read, trim_mode: '%-').result(binding)
-        puts text
+      File.open('./index.html', 'w') do |f1|
+        f1.puts text
+      end
     end
   end
 end
